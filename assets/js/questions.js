564ccf457b87145e8080f6b0fc24e150c5f6b73b
Meg Walker <meg.walker56@gmail.com>
2023-07-29 22:13:06 +0100

    added to HTML file for quiz questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -2,55 +2,41 @@
 var timeCount = document.querySelector(".timer");
 var time = document.querySelector("#time");
 var start = document.querySelector(".start-screen");
-var startButton = document.querySelector(".start");
+var startButton = document.querySelector("#start");
 var questionsCard = document.querySelector("#questions");
 var questionChoices = document.querySelector("#choices");
-
+var questionTitles = document.querySelector("#question-title");
 var end = document.querySelector("#end-screen");
 
+// startButton.addEventListener("click", timeCountdown());
 //Countdown function
-
 var timeLeft = 90;
+var timeStart = 0;
 
 time.innerHTML = timeLeft;
 
 function timeCountdown(seconds) {
   timeLeft = seconds;
-  //   time.innerHTML = timeLeft;
   var countDown = setInterval(() => {
     time.innerHTML = timeLeft;
     console.log(timeLeft);
     timeLeft--;
     if (timeLeft < 0) {
       clearInterval(countDown);
-      console.log(stop);
+      console.log("stop");
     }
+    // if ((countDown = "click")) {
+    //   setInterval(countDown);
+    //   console.log("start");
+    // }
   }, 1000);
-
-  return;
 }
 
 timeCountdown(90);
 
-//Create event listener to initiate start of quiz once button has been pressed - link to bottom of page
+//Create elements for the quiz
 
-//Target ''timer'' class in html script/ use setInterval() function
-// countDown = setTimeout(timeOut, 9000);
-
-// function timeOut() {
-//   document.getElementById("end-screen").innerHTML = "All Done!";
-// }
-// console.log(countDown);
-//give user timer of 60 to 90 seconds
-
-//Function to present questions on the page
-//Target "Questions" ID - class of hide - review how to 'hide' and 'reveal' elements.
-
-//May need to create elements/apend for the questions and answers
-
-//Function to select a series of choices. Class of "hide" again.
-//Structure for multiple choice selections
-//Alert when the answer is correct/incorrect
+//set function to toggle and hide elements
 
 //Function/if else statements to determine what happens if user selects correct and incorrect answers
 //if user (answer = true) - user + 1 point
@@ -64,3 +50,5 @@ timeCountdown(90);
 //Display of user's score
 
 //Function to log the scores of user in browser
+
+//start button event listener
