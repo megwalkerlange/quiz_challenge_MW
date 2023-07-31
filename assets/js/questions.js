7e996b7ebd17ed22132a3d559d1c98baf68cd034
Meg Walker <meg.walker56@gmail.com>
2023-07-31 15:26:50 +0100

    tidied up existing code - stopped lag on countdown - trying to create function to move ot next question using for loop

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,57 +1,66 @@
 //Create variables to target the elements in the document
+//initial variables created
 var timeCount = document.querySelector(".timer");
 var time = document.querySelector("#time");
 var start = document.querySelector(".start-screen");
 var startButton = document.querySelector("#start");
 var questionsCard = document.querySelector("#questions");
 var questionChoices = document.querySelector("#choices");
-var questionTitles = document.querySelector("#question-title");
+var questionBlocks = document.querySelector("#question-title");
 var end = document.querySelector("#end-screen");
+
+//variables created for the click event on questions
 correct = document.getElementById("true");
 incorrect = document.getElementById("false");
 incorrectAlert = document.getElementById("incorrect");
 correctAlert = document.getElementById("correct");
 
+//variables for time/score
 var timeLeft = 90;
-var timeStart = 0;
+var score = 0;
 
-time.innerHTML = timeLeft;
+//variables for individual questions hide/reveal
 
-function timeCountdown(seconds) {
-  timeLeft = seconds;
-  var countDown = setInterval(() => {
-    time.innerHTML = timeLeft;
-    console.log(timeLeft);
-    timeLeft--;
-    if (timeLeft < 0) {
-      clearInterval(countDown);
-      console.log("stop");
-    }
-    // if ((countDown = "click")) {
-    //   setInterval(countDown);
-    //   console.log("start");
-    // }
-  }, 1000);
-}
+var questionOne = document.getElementsByClassName("question-one");
+var questionTwo = document.getElementsByClassName("question-two");
+var questionThree = document.getElementsByClassName("question-three");
+var questionFour = document.getElementsByClassName("question-four");
+var questionFive = document.getElementsByClassName("question-five");
 
-timeCountdown(90);
-//function to show questions and hide start page
-function startQuiz(q) {
+//create array for question variables
+
+var questionList = [];
+
+questionList.push(questionOne);
+questionList.push(questionTwo);
+questionList.push(questionThree);
+questionList.push(questionFour);
+questionList.push(questionFive);
+
+console.log(questionList);
+
+//function to hide start screen and show first question when start button is clicked
+startButton.addEventListener("click", function startQuiz(q) {
   var q = document.getElementById("questions");
   var hide = document.getElementById("start-screen");
   if (q.style.display === "none") {
     (q.style.display = "block") + (hide.className = "hide");
   } else {
     (q.style.display = "none") + (hide.className = "start");
+    var countdown = setInterval(() => {
+      time.innerHTML = timeLeft;
+      console.log(timeLeft);
+      timeLeft--;
+      if (timeLeft <= 0) {
+        clearInterval(countdown);
+        console.log("stop");
+      }
+    }, 1000);
   }
   return;
-}
+});
 
-// correct = document.getElementById("true");
-// incorrect = document.getElementById("false");
-// incorrectAlert = document.getElementById("incorrect");
-// correctAlert = document.getElementById("correct");
-
+//function to set alert on page when incorrect anser is selected
 incorrect.addEventListener("click", function wrongAnswers() {
   if ((incorrect.value = "false")) {
     incorrectAlert.className = "start";
@@ -61,6 +70,7 @@ incorrect.addEventListener("click", function wrongAnswers() {
   }
 });
 
+//function to set alert on page when correct answer is selected
 correct.addEventListener("click", function correctAnswers() {
   if ((correct.value = "true")) {
     correctAlert.className = "start";
@@ -69,6 +79,23 @@ correct.addEventListener("click", function correctAnswers() {
     }, 3000);
   }
 });
+
+correct.addEventListener("click", function logScore() {
+  if ((correct.value = "true")) {
+    score++;
+    console.log(score);
+  }
+  return;
+});
+
+// correct.addEventListener("click", function changeQuestion() {
+//   for (let i = 0; i < questionList.length; i++) {
+//     if ((correct.value = "true")) {
+//       questionList.className = "hide";
+//     }
+//   }
+//   return;
+// });
 
 // questionChoices.addEventListener("click", quizAnswers);
 //show questions one by one as user selects questions
