9d5fec0b2825b9190c1ef6b9b21812fd65854191
Meg Walker <meg.walker56@gmail.com>
2023-07-31 22:18:30 +0100

    code after tutoring session

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ I used code/solutions from the sources below to help me in this challenge:
 
 lines 37 - 44 in questions.js
 https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_toggle_hide_show
+
+Lines 62 - 78 - Code refactored and a solution on how to change between questions after tutoring session with Jaytee Padilla.
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -2,7 +2,7 @@
 //initial variables created
 var timeCount = document.querySelector(".timer");
 var time = document.querySelector("#time");
-var start = document.querySelector(".start-screen");
+var start = document.querySelector("#start-screen");
 var startButton = document.querySelector("#start");
 var questionsCard = document.querySelector("#questions");
 var questionChoices = document.querySelector("#choices");
@@ -10,8 +10,9 @@ var questionBlocks = document.querySelector("#question-title");
 var end = document.querySelector("#end-screen");
 
 //variables created for the click event on questions
-correct = document.getElementById("true");
-incorrect = document.getElementById("false");
+// correct = document.getElementById("true");
+// incorrect = document.getElementById("false");
+var answerButtons = document.querySelectorAll(".choice");
 incorrectAlert = document.getElementById("incorrect");
 correctAlert = document.getElementById("correct");
 
@@ -21,64 +22,81 @@ var score = 0;
 
 //variables for individual questions hide/reveal
 
-var questionOne = document.getElementsByClassName("question-one");
-var questionTwo = document.getElementsByClassName("question-two");
-var questionThree = document.getElementsByClassName("question-three");
-var questionFour = document.getElementsByClassName("question-four");
-var questionFive = document.getElementsByClassName("question-five");
+var questionOne = document.getElementById("questions-1");
+var questionTwo = document.getElementById("questions-2");
+var questionThree = document.getElementById("questions-3");
+var questionFour = document.getElementById("questions-4");
+var questionFive = document.getElementById("questions-5");
+
+var questionTracker = 1;
 
 //create array for question variables
 
-var questionList = [];
+// var questionList = [];
 
-questionList.push(questionOne);
-questionList.push(questionTwo);
-questionList.push(questionThree);
-questionList.push(questionFour);
-questionList.push(questionFive);
+// questionList.push(questionOne);
+// questionList.push(questionTwo);
+// questionList.push(questionThree);
+// questionList.push(questionFour);
+// questionList.push(questionFive);
 
-console.log(questionList);
+// console.log(questionList);
 
 //function to hide start screen and show first question when start button is clicked
-startButton.addEventListener("click", function startQuiz(q) {
-  var q = document.getElementById("questions");
-  var hide = document.getElementById("start-screen");
-  if (q.style.display === "none") {
-    (q.style.display = "block") + (hide.className = "hide");
-  } else {
-    (q.style.display = "none") + (hide.className = "start");
-    var countdown = setInterval(() => {
-      time.innerHTML = timeLeft;
-      console.log(timeLeft);
-      timeLeft--;
-      if (timeLeft <= 0) {
-        clearInterval(countdown);
-        console.log("stop");
-      }
-    }, 1000);
-  }
-  return;
+startButton.addEventListener("click", function startQuiz() {
+  start.className = "hide";
+  questionOne.style.display = "block";
+
+  var countdown = setInterval(() => {
+    time.innerHTML = timeLeft;
+    timeLeft--;
+    if (timeLeft <= 0) {
+      clearInterval(countdown);
+      console.log("stop");
+    }
+  }, 1000);
 });
 
 //function to set alert on page when incorrect anser is selected
-incorrect.addEventListener("click", function wrongAnswers() {
-  if ((incorrect.value = "false")) {
-    incorrectAlert.className = "start";
-    setInterval(() => {
-      incorrectAlert.className = "hide";
-    }, 3000);
-  }
-});
 
-//function to set alert on page when correct answer is selected
-correct.addEventListener("click", function correctAnswers() {
-  if ((correct.value = "true")) {
-    correctAlert.className = "start";
-    setInterval(() => {
-      correctAlert.className = "hide";
-    }, 3000);
-  }
-});
+for (let i = 0; i < answerButtons.length; i++) {
+  answerButtons[i].addEventListener("click", function (e) {
+    if (this.value === "correct") {
+      console.log("correct");
+    }
+
+    if (this.value === "incorrect") {
+      console.log("incorrect");
+    }
+
+    document.getElementById("questions-" + questionTracker).style.display =
+      "none";
+
+    questionTracker++;
+
+    document.getElementById("questions-" + questionTracker).style.display =
+      "block";
+  });
+}
+
+// incorrect.addEventListener("click", function wrongAnswers() {
+//   if ((incorrect.value = "false")) {
+//     incorrectAlert.className = "start";
+//     setInterval(() => {
+//       incorrectAlert.className = "hide";
+//     }, 3000);
+//   }
+// });
+
+// //function to set alert on page when correct answer is selected
+// correct.addEventListener("click", function correctAnswers() {
+//   if ((correct.value = "true")) {
+//     correctAlert.className = "start";
+//     setInterval(() => {
+//       correctAlert.className = "hide";
+//     }, 3000);
+//   }
+// });
 
 correct.addEventListener("click", function logScore() {
   if ((correct.value = "true")) {
@@ -89,12 +107,41 @@ correct.addEventListener("click", function logScore() {
 });
 
 // correct.addEventListener("click", function changeQuestion() {
+//   var questionOne = document.getElementById("questions-1");
+//   var questionTwo = document.getElementById("questions-2");
+//   var questionThree = document.getElementById("questions-3");
+//   var questionFour = document.getElementById("questions-4");
+//   var questionFive = document.getElementById("questions-5");
+
+//   var questionList = [];
+
+//   questionList.push(questionOne);
+//   questionList.push(questionTwo);
+//   questionList.push(questionThree);
+//   questionList.push(questionFour);
+//   questionList.push(questionFive);
+
+//   console.log(questionList);
 //   for (let i = 0; i < questionList.length; i++) {
 //     if ((correct.value = "true")) {
-//       questionList.className = "hide";
+//       (questionList[i + 1].style.display = "block") +
+//         (questionList[i].className = "hide");
 //     }
 //   }
 //   return;
+// });
+// i = 0;
+// for (let i = 0; i < questionList.length; i++);
+// console.log(questionList[i]);
+
+// correct.addEventListener("click", function changeQuestion() {
+//   var next = questionList[i++];
+//   var hide = questionList[i];
+//   if ((correct.value = "true")) {
+//     (next.style.display = "block") + (hide.className = "hide");
+//   } else {
+//     (next.style.display = "none") + (hide.className = "start");
+//   }
 // });
 
 // questionChoices.addEventListener("click", quizAnswers);
